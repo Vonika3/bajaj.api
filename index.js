6a2833581ee813316ab0245a90aca008cf4c2fa1
Aneesh <aneeshx000@gmail.com>
2024-03-17 10:30:10 +0530

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post("/bfhl", (req, res) => {
+  let { data } = req.body;
+  if (!data) return res.status(402).json("Data is required.");
+  const numbers = data.filter((item) => typeof item === "number");
+  const alpha = data.filter((item) => typeof item === "string");
+
+  response_text = {
+    isSuccess: true,
+    name: "Aneesh Kumar",
+    email: "aneesh1635.be21@chitkara.edu.in",
+    roll_no: 2110991635,
+    numbers: numbers,
+    alphabets: alpha
+  };
+
+  res.status(200).send(response_text);
+});
+
+app.listen(port, () => console.log(`Server started on ${port}`));
