e4aca42a5d6034bacb1d79f4750001d2acaee735
Aneesh <aneeshx000@gmail.com>
2024-03-17 11:31:08 +0530

    b

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const http = require("http");
 const express = require("express");
 const cors = require("cors");
 
+require("dotenv").config();
+
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
